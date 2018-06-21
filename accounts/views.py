d3a88a48602668d345afa63bb66d6756d2a8e8ec
Patrick Doherty <patrick.e.doherty@gmail.com>
2018-06-21 14:04:48 +0000

    User registration, validation backend added

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 def index(request):
@@ -39,5 +39,25 @@ def login(request):
     
 def registration(request):
     """Render the registration page"""
-    return render(request, 'registration.html')
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+        
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+        
+        if registration_form.is_valid():
+            registration_form.save()
+            
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password1'])
+            
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('index'))
+            else:
+                messages.error(request, "Unable to register your account at this time")
+    else:
+        registration_form = UserRegistrationForm()
+    return render(request, 'registration.html', {'registration_form': registration_form})
     
