41b2467e746d89748902806ef5fad2e59045f757
Patrick Doherty <patrick.e.doherty@gmail.com>
2018-06-21 14:55:22 +0000

    Backend User Profile display added

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from accounts.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
@@ -60,4 +61,9 @@ def registration(request):
     else:
         registration_form = UserRegistrationForm()
     return render(request, 'registration.html', {'registration_form': registration_form})
+    
+def user_profile(request):
+    """The user's profile page"""
+    user = User.objects.get(email=request.user.email)
+    return render(request, 'profile.html', {"profile": user})
     
